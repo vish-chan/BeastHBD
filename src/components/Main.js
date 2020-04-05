dcfa90fd840a35ceb744c5f1bdc6cfa550a7d0f5
Vishal <poison.iitr@gmail.com>
2020-04-05 14:57:01 +0530

    MainScreen

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,17 @@
 import React, {Component} from 'react';
 import {GIFS} from '../data/files'
+import ReactHowler from 'react-howler';
 
 const GifComponent = (props) => {
     return(
-        <div style={{position:'absolute', left: props.x, top: props.y}} className={props.css!=undefined? props.css: ""}>
-            <img src={props.src} alt={props.alt} />
+        <div style={{position:'absolute', left: props.x, top: props.y }} className={props.css!==undefined? props.css: ""}>
+            <img style={{width: props.scale!==undefined? props.scale:'100%', height: props.scale!==undefined? props.scale:'100%'}} src={props.src} alt={props.alt} />
         </div>
     );
 }
 
 const GifComponentList = (props) => {
-    const gifs = GIFS.map(gif => <GifComponent key={gif.id} src={gif.src} alt={gif.alt} x={gif.x} y={gif.y} css={gif.css}/>)
+    const gifs = GIFS.map(gif => <GifComponent key={gif.id} src={gif.src} alt={gif.alt} x={gif.x} y={gif.y} css={gif.css} scale={gif.scale}/>)
     
     return(
         <ul>
@@ -37,6 +38,7 @@ class MainComponent extends Component {
                         </div>
                     </div>
                 </div>
+                <ReactHowler src="audio/Cut_hbd.ogg" preload={true} loop={true} playing={false} />
             </div>
         );
     }
--- src/data/files.js
+++ src/data/files.js
@@ -14,30 +14,106 @@ export const GIFS = [
 
     {
         id: 1,
-        src: getGifPath('fw_2'),
-        x: 800,
-        y: 0,
+        src: getGifPath('fw_5'),
+        x: 400,
+        y: 200,
     },
     {
         id: 2,
-        src: getGifPath('fw_4'),
+        src: getGifPath('fw_6'),
         x: 0,
         y: 400,
     },
 
     {
         id: 3,
+        src: getGifPath('fw_5'),
+        x: 800,
+        y: 400,
+    },
+    {
+        id: 4,
+        src: getGifPath('fw_3'),
+        x: 1200,
+        y: 0,
+    },
+    {
+        id: 5,
+        src: getGifPath('fw_1'),
+        x: 0,
+        y: 550,
+    },
+
+    {
+        id: 6,
+        src: getGifPath('fw_6'),
+        x: 1400,
+        y: 500,
+    },
+
+    {
+        id: 7,
+        src: getGifPath('fw_1'),
+        x: 800,
+        y: 0,
+    },
+
+    {
+        id: 15,
+        src: getGifPath('fw_1'),
+        x: 1000,
+        y: 100,
+    },
+
+    {
+        id: 8,
         src: getGifPath('cat_1'),
         x: 1300,
         y: 500,
     },
     
     {
-        id: 4,
+        id: 9,
         src: getImgPath('cat_5'),
         x: 700,
         y: '100%',
         css: 'arriveBottom'
     },
 
+    {
+        id: 10,
+        src: getImgPath('modi'),
+        x: 700,
+        y: '100%',
+        css: 'modi'
+    },
+    {
+        id: 11,
+        src: getGifPath('nibba'),
+        x: 1400,
+        y: 0,
+        scale: '80%',
+    },
+    {
+        id: 12,
+        src: getGifPath('cat_7'),
+        x: 0,
+        y: 0,
+    },
+
+    {
+        id: 13,
+        src: getGifPath('cake_2'),
+        x: 500,
+        y: 0,
+        scale: '50%',
+    },
+    
+    {
+        id: 14,
+        src: getGifPath('gift_1'),
+        x: 900,
+        y: 0,
+        scale: '50%',
+    },
 ]
