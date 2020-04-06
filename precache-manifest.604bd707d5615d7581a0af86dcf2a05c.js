570fe83c22f96de0ac011762839f56e63c0431c4
Vishal <poison.iitr@gmail.com>
2020-04-06 22:15:54 +0530

    Updates

---
+++ precache-manifest.604bd707d5615d7581a0af86dcf2a05c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c459e1573640daf5c25a696992041e4",
+    "url": "/BeastHBD/index.html"
+  },
+  {
+    "revision": "bfcfca31ec7caeeff3f5",
+    "url": "/BeastHBD/static/css/2.aff48906.chunk.css"
+  },
+  {
+    "revision": "6ab2e9ab2a9885375c11",
+    "url": "/BeastHBD/static/css/main.80927810.chunk.css"
+  },
+  {
+    "revision": "bfcfca31ec7caeeff3f5",
+    "url": "/BeastHBD/static/js/2.3c9eca2f.chunk.js"
+  },
+  {
+    "revision": "f7fe43cf5d12b7c6064f2a7e415de116",
+    "url": "/BeastHBD/static/js/2.3c9eca2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ab2e9ab2a9885375c11",
+    "url": "/BeastHBD/static/js/main.ac8534ae.chunk.js"
+  },
+  {
+    "revision": "48edce39d0e1a1f3437f",
+    "url": "/BeastHBD/static/js/runtime-main.bf9981bc.js"
+  }
+]);
